fe284e19cf890c30c38827b152f0352b48df5a5e
UnsoldRB <takuma2977@yahoo.co.jp>
2024-01-12 22:06:02 +0900

    最適化

--- src/main/java/com/unsoldriceball/dimensionidgetter/Main.java
+++ src/main/java/com/unsoldriceball/dimensionidgetter/Main.java
@@ -17,50 +17,64 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
+
+
+
 @Mod(modid = Main.MOD_ID, acceptableRemoteVersions = "*")
-public class Main {
+public class Main
+{
     public static final String MOD_ID = "dimensionidgetter";
     public static final String MOD_NAME = "dimensionIDGetter";
 
+
+
+
+    //ModがInitializeを呼び出す前に発生するイベント。
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event){ //ModがInitializeを呼び出す前に発生するイベント。
-        MinecraftForge.EVENT_BUS.register(this); //これでこのクラス内でForgeのイベントが動作するようになるらしい。
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        //これでこのクラス内でForgeのイベントが動作するようになるらしい。
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
 
 
+    //プレイヤーがワールドにログインしたときのイベント。
     @SubscribeEvent
-    public void enterWorld(PlayerEvent.PlayerLoggedInEvent event) { //プレイヤーがワールドにログインしたときのイベント。
-        final Map<DimensionType, IntSortedSet> DIMENSIONS = DimensionManager.getRegisteredDimensions(); //登録されたDimension一覧を取得
-        Set<Integer> dimensionIDs = new TreeSet<>();
+    public void enterWorld(PlayerEvent.PlayerLoggedInEvent event)
+    {
+        final Map<DimensionType, IntSortedSet> LDIMENSIONS = DimensionManager.getRegisteredDimensions();    //登録されたDimension一覧を取得
+        Set<Integer> dimensionIDs = new TreeSet<>();                                                        //登録されたDimensionIDの一覧が入る
 
-        for (IntSortedSet idSet : DIMENSIONS.values()) { //全てのDimensionIDをSetに格納していく。
-            dimensionIDs.addAll(idSet);
+
+        //全てのDimensionIDをSetに格納していく。
+        for (IntSortedSet _idSet : LDIMENSIONS.values())
+        {
+            dimensionIDs.addAll(_idSet);
         }
 
-        String dim_id = ""; //1ループごとにObjectからStringに変換されたDimensionIDが入る。
-        StringBuilder dim_comma = new StringBuilder(); //コンマで区切られたDimensionIDを組み立てる。
-        StringBuilder dim_indention = new StringBuilder(); //改行で区切られたDimensionIDを組み立てる。
-        final String INDENTION = System.getProperty("line.separator"); //改行コード
+        final String LINDENTION = System.getProperty("line.separator");     //改行コード
+        StringBuilder dim_comma = new StringBuilder();                      //コンマで区切られたDimensionIDを組み立てる。
+        StringBuilder dim_indention = new StringBuilder();                  //改行で区切られたDimensionIDを組み立てる。
 
-        for (int i : dimensionIDs) { //全てのDimensionIDをSetに格納していく。
-            dim_id = String.valueOf(i);
+        //全てのDimensionIDをSetに格納していく。
+        for (int _i : dimensionIDs)
+        {
+            String _dim_id = String.valueOf(_i);        //ObjectからStringに変換されたDimensionIDが入る。
 
-            dim_comma.append(dim_id).append(", ");
-            dim_indention.append(dim_id).append(INDENTION);
+            dim_comma.append(_dim_id).append(", ");
+            dim_indention.append(_dim_id).append(LINDENTION);
         }
         //それぞれ最後のコンマと改行コードを取り除いて結合し、{}を取り除く。
-        final String RESULT_COMMA = dim_comma.substring(0, dim_comma.length() - 2);
-        final String RESULT_INDENTION = dim_indention.substring(0, dim_indention.length() - 2);
-        String result = RESULT_COMMA + INDENTION + INDENTION + RESULT_INDENTION;
+        final String LRESULT_COMMA = dim_comma.substring(0, dim_comma.length() - 2);
+        final String LRESULT_INDENTION = dim_indention.substring(0, dim_indention.length() - 2);
+        String result = LRESULT_COMMA + LINDENTION + LINDENTION + LRESULT_INDENTION;
 
-        final String GAMEDIR = Loader.instance().getConfigDir().getParentFile().getAbsolutePath(); //ファイル生成先のディレクトリ
+        final String LGAMEDIR = Loader.instance().getConfigDir().getParentFile().getAbsolutePath();     //ファイル生成先のディレクトリ
+        final String LFILEPATH = LGAMEDIR + File.separator + MOD_NAME + ".txt";
+        File file = new File(LFILEPATH);                                                                //ファイル作成
+        FileWriter writer;
 
-
-        final String FILEPATH = GAMEDIR + File.separator + MOD_NAME + ".txt";
-        File file = new File(FILEPATH); //ファイル作成
-
-        FileWriter writer = null;
         try {
             writer = new FileWriter(file);
             writer.write(result);
